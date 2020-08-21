783f382ccfd504271a96679c85e3826e7843ea6f
Will Lytle <wlytle89@gmail.com>
2020-08-21 22:34:19 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,10 +1,12 @@
 require 'pry'
 def find_item_by_name_in_collection(name, collection)
+  desired_item = nil
   collection.each do |grocery|
       if grocery[:item] === name
-        return grocery
+        desired_item = grocery
       end
   end
+  desired_item
 end
 
 def consolidate_cart(cart)
